9d17474aa1a3fd850042b8933cad8af7a444f9de
Carlos <toxz@foxmail.com>
2016-03-22 16:35:34 +0800

    define constants

---
+++ app/src/main/java/co/yishun/lighting/Constants.java
@@ -0,0 +1,9 @@
+package co.yishun.lighting;
+
+/**
+ * Created by Carlos on 2016/3/22.
+ */
+public class Constants {
+    public static final boolean LOG_ENABLE = false;
+    public static final boolean SANDBOX = false;
+}
