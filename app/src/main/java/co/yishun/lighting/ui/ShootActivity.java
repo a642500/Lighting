0e86e1d74906b6d9bc8d9c763e6aee2f8dd420ce
Carlos <toxz@foxmail.com>
2016-03-24 14:05:57 +0800

    add page indicator dot

--- app/src/main/java/co/yishun/lighting/ui/ShootActivity.java
+++ app/src/main/java/co/yishun/lighting/ui/ShootActivity.java
@@ -9,15 +9,30 @@ import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
+import java.io.File;
+
 import co.yishun.lighting.R;
+import co.yishun.lighting.function.Callback;
+import co.yishun.lighting.function.Consumer;
+import co.yishun.lighting.ui.view.PageIndicatorDot;
+import co.yishun.lighting.ui.view.shoot.CameraGLSurfaceView;
+import co.yishun.lighting.ui.view.shoot.IShootView;
+import co.yishun.lighting.ui.view.shoot.filter.FilterManager;
 
 /**
  * Created by Carlos on 2016/3/22.
  */
 @EActivity
-public class ShootActivity extends BaseActivity {
+public class ShootActivity extends BaseActivity implements Callback, Consumer<File>, IShootView.SecurityExceptionHandler {
     @ViewById
     Toolbar toolbar;
+    @ViewById
+    IShootView shootView;
+    @ViewById(R.id.pageIndicator)
+    PageIndicatorDot pageIndicatorDot;
+    @Nullable
+    private CameraGLSurfaceView mCameraGLSurfaceView;
+    private boolean flashOn = false;
 
     @Override
     public String getPageInfo() {
@@ -30,13 +45,35 @@ public class ShootActivity extends BaseActivity {
         setContentView(R.layout.activity_shoot);
 
 
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                exitClicked();
-            }
+        toolbar.setNavigationOnClickListener(v -> exitClicked());
+
+        shootView.setSecurityExceptionHandler(this);
+        if (shootView instanceof CameraGLSurfaceView) {
+            mCameraGLSurfaceView = ((CameraGLSurfaceView) shootView);
+            pageIndicatorDot = ((PageIndicatorDot) findViewById(R.id.pageIndicator));
+            pageIndicatorDot.setNum(FilterManager.FilterType.values().length);
+
+            mCameraGLSurfaceView.setFilterListener(index -> pageIndicatorDot.setCurrent(index));
+        }
+
+        View shootBtn = findViewById(R.id.shootBtn);
+        shootBtn.setOnClickListener(v -> {
+            //                    shootBtn.setDuration(1100);
+            shootView.record(this, this);
         });
+
+        setControllerBtn();
     }
+
+
+    @Override
+    protected void onResume() {
+        if (mCameraGLSurfaceView != null) {
+            mCameraGLSurfaceView.onResume();
+        }
+        super.onResume();
+    }
+
 
     void exitClicked() {
         this.finish();
@@ -54,6 +91,21 @@ public class ShootActivity extends BaseActivity {
 
     @Click
     void shootBtnClicked() {
+
+    }
+
+    @Override
+    public void call() {
+
+    }
+
+    @Override
+    public void accept(File file) {
+
+    }
+
+    @Override
+    public void onHandler(SecurityException e) {
 
     }
 }
