afe38d94e8a1195c66e93ada361d3608f7d4dda3
Carlos <toxz@foxmail.com>
2016-03-24 16:15:33 +0800

    finish up shoot activity

--- app/src/main/java/co/yishun/lighting/ui/ShootActivity.java
+++ app/src/main/java/co/yishun/lighting/ui/ShootActivity.java
@@ -1,12 +1,19 @@
 package co.yishun.lighting.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageSwitcher;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
 
 import java.io.File;
@@ -18,18 +25,26 @@ import co.yishun.lighting.ui.view.PageIndicatorDot;
 import co.yishun.lighting.ui.view.shoot.CameraGLSurfaceView;
 import co.yishun.lighting.ui.view.shoot.IShootView;
 import co.yishun.lighting.ui.view.shoot.filter.FilterManager;
+import co.yishun.lighting.util.FileUtil;
+import co.yishun.lighting.util.LogUtil;
+import co.yishun.lighting.util.video.VideoCommand;
+import co.yishun.lighting.util.video.VideoConvert;
 
 /**
  * Created by Carlos on 2016/3/22.
  */
 @EActivity
 public class ShootActivity extends BaseActivity implements Callback, Consumer<File>, IShootView.SecurityExceptionHandler {
+    private static final String TAG = "ShootActivity";
     @ViewById
     Toolbar toolbar;
-    @ViewById
     IShootView shootView;
     @ViewById(R.id.pageIndicator)
     PageIndicatorDot pageIndicatorDot;
+    @ViewById
+    View cameraSwitch;
+    @ViewById
+    ImageSwitcher recordFlashSwitch;
     @Nullable
     private CameraGLSurfaceView mCameraGLSurfaceView;
     private boolean flashOn = false;
@@ -45,6 +60,7 @@ public class ShootActivity extends BaseActivity implements Callback, Consumer<Fi
         setContentView(R.layout.activity_shoot);
 
 
+        shootView = (IShootView) findViewById(R.id.shootView);
         toolbar.setNavigationOnClickListener(v -> exitClicked());
 
         shootView.setSecurityExceptionHandler(this);
@@ -56,15 +72,35 @@ public class ShootActivity extends BaseActivity implements Callback, Consumer<Fi
             mCameraGLSurfaceView.setFilterListener(index -> pageIndicatorDot.setCurrent(index));
         }
 
-        View shootBtn = findViewById(R.id.shootBtn);
-        shootBtn.setOnClickListener(v -> {
-            //                    shootBtn.setDuration(1100);
-            shootView.record(this, this);
-        });
-
-        setControllerBtn();
+        setControlBtn();
     }
 
+    @Click
+    void shootBtnClicked() {
+        shootView.record(this, this);
+        //TODO update record btn and other btns
+    }
+
+    private void setControlBtn() {
+        flashOn = false;
+        cameraSwitch.setEnabled(shootView.isFrontCameraAvailable());
+        recordFlashSwitch.setEnabled(shootView.isFlashlightAvailable());
+        recordFlashSwitch.setDisplayedChild(0);
+    }
+
+    @Click
+    void cameraSwitchClicked(View view) {
+        shootView.switchCamera(!shootView.isBackCamera());
+        setControlBtn();
+    }
+
+    @Click
+    void recordFlashSwitchClicked(View view) {
+        LogUtil.i(TAG, "flashlight switch, from " + flashOn + " to " + !flashOn);
+        flashOn = !flashOn;
+        shootView.setFlashlightOn(flashOn);
+        recordFlashSwitch.setDisplayedChild(flashOn ? 1 : 0);
+    }
 
     @Override
     protected void onResume() {
@@ -74,38 +110,103 @@ public class ShootActivity extends BaseActivity implements Callback, Consumer<Fi
         super.onResume();
     }
 
+    @Override
+    protected void onPause() {
+        if (mCameraGLSurfaceView != null) {
+            mCameraGLSurfaceView.onPause();
+        }
+        super.onPause();
+        this.finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mCameraGLSurfaceView != null) {
+            mCameraGLSurfaceView.onDestroy();
+        }
+        super.onDestroy();
+    }
+
 
     void exitClicked() {
         this.finish();
     }
 
-    @Click
-    void flashlightSwitchClicked() {
-
-    }
-
-    @Click
-    void cameraSwitchClicked() {
-
-    }
-
-    @Click
-    void shootBtnClicked() {
-
-    }
-
     @Override
     public void call() {
-
+        LogUtil.i(TAG, "start record callback");
     }
 
     @Override
     public void accept(File file) {
+        LogUtil.i(TAG, "accept: " + file);
+        if (shootView instanceof CameraGLSurfaceView)
+            delayStart(file);
+        else {
+            showProgress();
+            delayAccept(file);
+        }
+    }
 
+    @UiThread(delay = 800)
+    void delayAccept(File file) {
+        File newFile = FileUtil.getVideoCacheFile(this);
+        new VideoConvert(this).setFiles(file, newFile).setListener(new VideoCommand.VideoCommandListener() {
+            @Override
+            public void onSuccess(VideoCommand.VideoCommandType type) {
+                file.delete();
+                delayStart(newFile);
+                hideProgress();
+            }
+
+            @Override
+            public void onFail(VideoCommand.VideoCommandType type) {
+                hideProgress();
+            }
+        }).start();
+    }
+
+    @UiThread(delay = 200)
+    void delayStart(File file) {
+//        TagCreateActivity_.intent(this).forDiary(forDiary).forWorld(forWorld).forToday(forToday).world(world).videoPath(file.getPath()).start();
+        this.finish();
     }
 
     @Override
     public void onHandler(SecurityException e) {
+        LogUtil.e(TAG, "", e);
+        MaterialDialog.Builder builder = new MaterialDialog.Builder(this).positiveText(R.string.activity_shoot_permission_error_ok).content(R.string.activity_shoot_permission_error_msg).title(R.string.activity_shoot_permission_error_title).cancelable(false);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            builder.negativeText(R.string.activity_shoot_permission_error_settings);
+            builder.callback(new MaterialDialog.ButtonCallback() {
+                @Override
+                public void onPositive(MaterialDialog dialog) {
+                    ShootActivity.this.finish();
+                }
 
+                @Override
+                public void onNegative(MaterialDialog dialog) {
+                    try {
+                        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:" + getPackageName()));
+                            intent.addCategory(Intent.CATEGORY_DEFAULT);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        }
+                    } catch (Exception ignore) {
+                        ignore.printStackTrace();
+                    }
+                }
+            });
+        } else {
+            builder.callback(new MaterialDialog.ButtonCallback() {
+                @Override
+                public void onPositive(MaterialDialog dialog) {
+                    ShootActivity.this.finish();
+                }
+            });
+        }
+        builder.show();
+        //TODO add help btn to guide user to how enable permission for three-party rom
     }
 }
