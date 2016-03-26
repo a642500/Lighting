6ba0c62ec32f0ba3a0c0496f41fa53c3ce83b460
Carlos <toxz@foxmail.com>
2016-03-26 21:25:15 +0800

    add base url

--- app/src/main/java/co/yishun/lighting/Constants.java
+++ app/src/main/java/co/yishun/lighting/Constants.java
@@ -22,6 +22,10 @@ public class Constants {
     public static final int VIDEO_WIDTH = 640;
     public static final int VIDEO_FPS = 30;
 
-    public static final String BASE_URL = "";
+    public static final String BASE_URL;
+
+    static {
+        BASE_URL = SANDBOX ? "http://devlightup.hustonline.net/v1" : "http://lightup.hustonline.net/v1";
+    }
 
 }
