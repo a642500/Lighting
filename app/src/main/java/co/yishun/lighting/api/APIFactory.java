5b824cd63833558b6421f4f2af38d16fc456bf54
Carlos <toxz@foxmail.com>
2016-03-28 18:38:29 +0800

    upate api

--- app/src/main/java/co/yishun/lighting/api/APIFactory.java
+++ app/src/main/java/co/yishun/lighting/api/APIFactory.java
@@ -1,16 +1,23 @@
 package co.yishun.lighting.api;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.GsonBuilder;
+
 import co.yishun.lighting.Constants;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by carlos on 3/26/16.
  */
 public class APIFactory {
 
-    private static Retrofit mRetrofit = new Retrofit.Builder().baseUrl(Constants.BASE_URL).build();
+    private static Retrofit mRetrofit = new Retrofit.Builder().baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                    .create())).build();
 
-    public Account getAccountAPI() {
+    public static Account getAccountAPI() {
         return mRetrofit.create(Account.class);
     }
 }
