34651ad98925ba90b3337b6f215ca774cd592490
Carlos <toxz@foxmail.com>
2016-03-28 18:40:13 +0800

    remove useless code

--- app/src/main/java/co/yishun/lighting/ui/LoginActivity.java
+++ app/src/main/java/co/yishun/lighting/ui/LoginActivity.java
@@ -36,23 +36,10 @@ import retrofit2.Response;
 @EActivity(R.layout.activity_login)
 public class LoginActivity extends AppCompatActivity {
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
     @ViewById
     ViewPager viewPager;
     @ViewById
     Button signInBtn;
-    // UI references.
     @ViewById
     EditText phoneEditText;
     @ViewById
