b04a8c61b9a0eddadc74ec7d34ab85e8f307abcd
Carlos <toxz@foxmail.com>
2016-03-29 00:08:15 +0800

    update APIFactory

--- app/src/main/java/co/yishun/lighting/api/APIFactory.java
+++ app/src/main/java/co/yishun/lighting/api/APIFactory.java
@@ -4,6 +4,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.GsonBuilder;
 
 import co.yishun.lighting.Constants;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -15,9 +16,13 @@ public class APIFactory {
     private static Retrofit mRetrofit = new Retrofit.Builder().baseUrl(Constants.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create(new GsonBuilder()
                     .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                    .create())).build();
+                    .create())).client(getOkHttpClient()).build();
 
     public static Account getAccountAPI() {
         return mRetrofit.create(Account.class);
+    }
+
+    public static OkHttpClient getOkHttpClient() {
+        return new OkHttpClient.Builder().build();
     }
 }
