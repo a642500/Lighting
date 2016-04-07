e4da7109ce950a985bed63be63a421db15867fbf
Carlos <toxz@foxmail.com>
2016-04-07 09:11:09 +0800

    add Deserializer for gson converter

--- app/src/main/java/co/yishun/lighting/api/APIFactory.java
+++ app/src/main/java/co/yishun/lighting/api/APIFactory.java
@@ -3,8 +3,16 @@ package co.yishun.lighting.api;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
 
 import co.yishun.lighting.Constants;
+import co.yishun.lighting.api.model.Token;
+import co.yishun.lighting.api.model.User;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -13,12 +21,18 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created by carlos on 3/26/16.
  */
 public class APIFactory {
-    private static Gson mGson = new GsonBuilder()
+    private final static Gson mGson = new GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create();
+
+    private final static Gson mDeserializerGson = new GsonBuilder()
+            .registerTypeAdapter(User.class, new DataDeserializer<>())
+            .registerTypeAdapter(Token.class, new DataDeserializer<>())
             .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
             .create();
 
     private static Retrofit mRetrofit = new Retrofit.Builder().baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(mGson)).client(getOkHttpClient()).build();
+            .addConverterFactory(GsonConverterFactory.create(mDeserializerGson)).client(getOkHttpClient()).build();
 
     public static Account getAccountAPI() {
         return mRetrofit.create(Account.class);
@@ -27,4 +41,18 @@ public class APIFactory {
     public static OkHttpClient getOkHttpClient() {
         return new OkHttpClient.Builder().build();
     }
+
+    public static class DataDeserializer<T> implements JsonDeserializer<T> {
+
+        @Override
+        public T deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            // Get the "content" element from the parsed JSON
+            JsonElement content = json.getAsJsonObject().get("data");
+
+            // Deserialize it. You use a new instance of Gson to avoid infinite recursion
+            // to this deserializer
+            return mGson.fromJson(content, typeOfT);
+        }
+    }
+
 }
--- app/src/main/java/co/yishun/lighting/api/model/Token.java
+++ app/src/main/java/co/yishun/lighting/api/model/Token.java
@@ -6,35 +6,14 @@ import java.io.Serializable;
  * Created by carlos on 4/6/16.
  */
 public class Token implements Serializable {
-    private Data data;
+    public String userId;
+    public String accessToken;
 
     @Override
     public String toString() {
         return "Token{" +
-                "userId='" + data.userId + '\'' +
-                ", accessToken='" + data.accessToken + '\'' +
+                "userId='" + userId + '\'' +
+                ", accessToken='" + accessToken + '\'' +
                 '}';
-    }
-
-    public String getAccessToken() {
-        return data.accessToken;
-    }
-
-    public void setAccessToken(String accessToken) {
-        data.accessToken = accessToken;
-    }
-
-    public String getUserId() {
-        return data.userId;
-    }
-
-    public void setUserId(String userId) {
-        data.userId = userId;
-    }
-
-
-    private static class Data {
-        public String userId;
-        public String accessToken;
     }
 }
