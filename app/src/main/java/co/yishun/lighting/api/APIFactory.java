84dddd48ba0124bd554cbb3dad9811a77b6981e3
Carlos <toxz@foxmail.com>
2016-04-07 08:39:53 +0800

    update api

--- app/src/main/java/co/yishun/lighting/api/APIFactory.java
+++ app/src/main/java/co/yishun/lighting/api/APIFactory.java
@@ -1,6 +1,7 @@
 package co.yishun.lighting.api;
 
 import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import co.yishun.lighting.Constants;
@@ -12,11 +13,12 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created by carlos on 3/26/16.
  */
 public class APIFactory {
+    private static Gson mGson = new GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create();
 
     private static Retrofit mRetrofit = new Retrofit.Builder().baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(new GsonBuilder()
-                    .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                    .create())).client(getOkHttpClient()).build();
+            .addConverterFactory(GsonConverterFactory.create(mGson)).client(getOkHttpClient()).build();
 
     public static Account getAccountAPI() {
         return mRetrofit.create(Account.class);
--- app/src/main/java/co/yishun/lighting/api/model/Token.java
+++ app/src/main/java/co/yishun/lighting/api/model/Token.java
@@ -6,14 +6,35 @@ import java.io.Serializable;
  * Created by carlos on 4/6/16.
  */
 public class Token implements Serializable {
-    public String userId;
-    public String accessToken;
+    private Data data;
 
     @Override
     public String toString() {
         return "Token{" +
-                "userId='" + userId + '\'' +
-                ", accessToken='" + accessToken + '\'' +
+                "userId='" + data.userId + '\'' +
+                ", accessToken='" + data.accessToken + '\'' +
                 '}';
+    }
+
+    public String getAccessToken() {
+        return data.accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        data.accessToken = accessToken;
+    }
+
+    public String getUserId() {
+        return data.userId;
+    }
+
+    public void setUserId(String userId) {
+        data.userId = userId;
+    }
+
+
+    private static class Data {
+        public String userId;
+        public String accessToken;
     }
 }
